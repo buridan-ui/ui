308c6b9dc96a252fe3b991e957182fdf422f7950
pourhakimi <84860195+pourhakimi@users.noreply.github.com>
2025-05-03 16:39:12 +0300

    clean up cards components

--- buridan_ui/pantry/cards/v3.py
+++ buridan_ui/pantry/cards/v3.py
@@ -6,9 +6,7 @@ def card_v3():
         rx.box(
             color=rx.color("gray", 4),
             class_name=(
-                # Dimesnions
                 "w-full h-64 "
-                # Placeholder Background
                 + "col-start-2 row-span-full row-start-1 bg-[size:10px_10px] bg-fixed bg-[image:repeating-linear-gradient(315deg,currentColor_0,currentColor_1px,_transparent_0,_transparent_50%)]"
             ),
         ),
@@ -16,18 +14,13 @@ def card_v3():
         rx.box(
             color=rx.color("gray", 4),
             class_name=(
-                # Dimesnions
                 "w-full h-12 "
-                # Placeholder Background
                 + "col-start-2 row-span-full row-start-1 bg-[size:10px_10px] bg-fixed bg-[image:repeating-linear-gradient(315deg,currentColor_0,currentColor_1px,_transparent_0,_transparent_50%)]"
             ),
         ),
         class_name=(
-            # Dimesnions
             "w-full max-w-[35em] "
-            # Layout
             + "flex flex-col p-4 gap-y-2 "
-            # Appearance
             + "rounded-md border border-dashed border-gray-600 "
         ),
     )
