ece9b827c6a018ce1f780eec1f16c8b9cfea96ce
Ahmad Hakim <pourhakimi@pm.me>
2025-07-25 08:26:25 +0300

    add goat tracker site analytics

--- buridan_ui/buridan_ui.py
+++ buridan_ui/buridan_ui.py
@@ -1,5 +1,15 @@
 import reflex as rx
 from buridan_ui.export import export_app
 
-app = rx.App(stylesheets=["css/globals.css"])
+app = rx.App(
+    stylesheets=["css/globals.css"],
+    head_components=[
+        rx.script(
+            src="https://gc.zgo.at/count.js",
+            custom_attrs={
+                "data-goatcounter": "https://buridan-ui.goatcounter.com/count",
+            },
+        )
+    ],
+)
 export_app(app)
