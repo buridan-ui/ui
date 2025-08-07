4e68c548cb6dc452a8b2eeed4ff3ff618a25530b
Ahmad Hakim <pourhakimi@pm.me>
2025-08-07 16:31:55 +0300

    rename source to src

---
+++ src/src.py
@@ -0,0 +1,15 @@
+import reflex as rx
+from src.export import export_app
+
+app = rx.App(
+    stylesheets=["css/globals.css"],
+    head_components=[
+        rx.script(
+            src="https://gc.zgo.at/count.js",
+            custom_attrs={
+                "data-goatcounter": "https://buridan-ui.goatcounter.com/count",
+            },
+        )
+    ],
+)
+export_app(app)
